908ac818e2450f9b47516f7d34b8abe1d8b214f6
Ryan Williams <ryrowi@gmail.com>
2016-09-04 16:37:59 -0400

    proof-of-concept api connectivity & template renderer

--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 var express = require('express');
 var app = express();
 
+var fs = require('fs'); // this engine requires the fs module
+
+var Weather = require('./node_modules/weather.js');
+
+'use strict';
+
+const ForecastIO = require('forecast-io')
+const forecast = new ForecastIO('XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX')
+
+app.engine('ntl', function (filePath, options, callback) { // define the template engine
+  fs.readFile(filePath, function (err, content) {
+    if (err) return callback(new Error(err));
+    // this is an extremely simple template engine
+    var rendered = content.toString().replace(/#([^#]+)#/, '$1');
+    return callback(null, rendered);
+  });
+});
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set('view engine', 'ntl');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+
+  forecast
+    .latitude('37.8267')
+    .longitude('-122.423')
+    .get()
+    .then(res => {
+      //console.log(res);
+      response.render('pages/index', JSON.parse(res));
+    })
+    .catch(err => {                 // handle your error response.
+      console.log(err)
+    })
+
 });
 
 app.listen(app.get('port'), function() {
