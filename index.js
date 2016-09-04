f36b161849c393fe445f8005ca911ee3385e709f
Ryan Williams <ryrowi@gmail.com>
2016-09-04 19:38:07 -0400

    remove invalid semicolon

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get('/:lat?/:lon?/:scale?', function(request, response) {
       response.render('pages/index', objectMerge(
         JSON.parse(res),
         {
-          scale: (units === 'si') ? 'C' : 'F';
+          scale: (units === 'si') ? 'C' : 'F'
         }
       ));
     })
