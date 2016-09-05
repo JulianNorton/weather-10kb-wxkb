3d96f1ef641e3979377c61b6ab31eb6e44879d22
Ryan Williams <ryrowi@gmail.com>
2016-09-05 13:41:59 -0400

    refactor, move app code into dedicated module

--- index.js
+++ index.js
@@ -1,113 +1,20 @@
-var compression = require('compression')
-var express = require('express')
-var favicon = require('serve-favicon')
-var forecastIO = require('forecast-io')        // coordinate-based forecast
-var freegeoip = require('node-freegeoip')     // ip-based geolocation
-var nodeGeocoder = require('node-geocoder')   // string-based geolocation e.g. "new york"
-var moment = require('moment-timezone')       // localize times in the forecast
-var objectMerge = require('object-merge')
-var timezone = require('google-timezone-api') // coordinate-based timezone
+var compression = require('compression');
+var express = require('express');
+var serveFavicon = require('serve-favicon');
+var moment = require('moment-timezone');
+var weather10kb = require('./weather10kb');
 
-var forecast = new forecastIO(process.env.FORECAST_IO_API_KEY)
+var app = express();
 
-var app = express()
+app.locals.moment = moment;
 
-var locateCtrl = function(request, response, next) {
+app.use(compression());
+app.use(express.static(__dirname + '/public'));
+app.use('/', weather10kb);
 
-  // check for & handle a querystring variable in case the user submitted the location form rather than passing a url param
-  if (typeof request.query.location === 'string') {
-    request.params.location = request.query.location
-  }
-
-  // if we receieved a location, geocode coordinates from it
-  if (typeof request.params.location === 'string') {
-
-    var geocoder = nodeGeocoder()
-
-    geocoder.geocode(request.params.location)
-      .then(function(res) {
-        request.params.lat = res[0].latitude
-        request.params.lon = res[0].longitude
-        return next()
-      })
-      .catch(function(err) {
-        console.log(err)
-        response.status(500).send('The location you entered (' + request.params.location + ') could not be geocoded.')
-      })
-
-  // otherwise guess coordinates based on ip
-  } else {
-
-    var ip = request.headers['x-forwarded-for'] ? request.headers['x-forwarded-for'].split(',')[0] : request.connection.remoteAddress
-
-    freegeoip.getLocation(ip, function(err, location) {
-      if (err) {
-        console.log(err)
-        response.status(500).send('Could not determine your location based on your IP (' + ip + ').')
-      } else {
-        console.log(location)
-        request.params.lat = location.latitude
-        request.params.lon = location.longitude
-        return next()
-      }
-    })
-
-  }
-
-}
-
-var forecastCtrl = function(request, response) {
-
-  var units = (typeof request.params.scale === 'string' && request.params.scale === 'C') ? 'si' : 'us'
-
-  var handleForecastResponse = function(res) {
-    var data = objectMerge(
-      JSON.parse(res),
-      {
-        scale: (units === 'si') ? 'C' : 'F',
-        params: request.params
-      }
-    )
-    response.render('pages/index', data)
-  }
-
-  var handleTimezoneResponse = function(res) {
-    request.params.tz = res.timeZoneId
-    moment.tz.setDefault(request.params.tz)
-
-    forecast
-      .latitude(request.params.lat)
-      .longitude(request.params.lon)
-      .units(units)
-      .get()
-      .then(handleForecastResponse)
-      .catch(err => {
-        console.log(err)
-        // TODO display message to user
-      })
-  }
-
-  timezone({location: request.params.lat + ',' + request.params.lon})
-    .then(handleTimezoneResponse)
-    .catch( function(err) {
-      console.log(err)
-      // TODO display message to user
-    })
-
-
-}
-
-app.locals.moment = moment
-
-app.use(compression())
-app.use(express.static(__dirname + '/public'))
-
-app.set('port', (process.env.PORT || 5000))
-app.set('view engine', 'ejs')
-
-app.get('/:location?', locateCtrl, forecastCtrl)
-app.get('/:lat/:lon/:scale?', forecastCtrl)
+app.set('port', (process.env.PORT || 5000));
+app.set('view engine', 'ejs');
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'))
-})
+  console.log('Node app is running on port', app.get('port'));
+});
---
+++ weather10kb.js
@@ -0,0 +1,107 @@
+var express       = require('express');
+var forecastIo    = require('forecast-io')
+var nodeFreegeoip = require('node-freegeoip')
+var nodeGeocoder  = require('node-geocoder')
+var moment        = require('moment-timezone')
+var objectMerge   = require('object-merge')
+var timezone      = require('google-timezone-api')
+
+var router = express.Router();
+
+function Weather10kbRequest(request) {
+  this.geocode = function() {
+    return new Promise(function(resolve, reject) {
+      if (typeof request.params.location === 'string') {
+        var geocoder = nodeGeocoder();
+
+        geocoder.geocode(request.params.location)
+          .then(function(res) {
+            request.params.latitude = res[0].latitude
+            request.params.longitude = res[0].longitude
+            resolve(request.params);
+          })
+          .catch(function(err) {
+            return reject(err);
+          })
+      } else {
+        var ip = this.headers['x-forwarded-for'] ? this.headers['x-forwarded-for'].split(',')[0] : this.connection.remoteAddress
+
+        freegeoip.getLocation(ip, function(err, location) {
+          if (err) return reject(err);
+          request.params.latitude = location.latitude
+          request.params.longitude = location.longitude
+          resolve();
+        })
+      }
+    });
+  }
+
+  this.setTimeZone = function() {
+    return new Promise(function(resolve, reject) {
+      timezone({location: request.params.latitude + ',' + request.params.longitude})
+        .then(function(res) {
+          request.params.tz = res.timeZoneId
+          moment.tz.setDefault(request.params.tz)
+          resolve();
+        })
+        .catch(function(err) {
+          return reject(err);
+        });
+    });
+  }
+
+  this.getForecast = function() {
+    return new Promise(function(resolve, reject) {
+      var forecast = new forecastIo(process.env.FORECAST_IO_API_KEY);
+      var units = (typeof request.params.scale === 'string' && request.params.scale === 'C') ? 'si' : 'us'
+
+      forecast
+        .latitude(request.params.latitude)
+        .longitude(request.params.longitude)
+        .units(units)
+        .get()
+        .then(function(res) {
+          resolve(res);
+        })
+        .catch(function(err) {
+          return reject(err);
+        });
+    });
+  }
+
+}
+
+router.get('/:location?/:scale?', function(request, response) {
+  // if we got a scale and not a location for the first param, adjust params accordingly
+  // TODO this should probably live in a validation function
+  if (request.params.location.toUpperCase() in ['C', 'F']) {
+    request.params.scale = request.params.location;
+    delete request.params.location;
+  }
+
+  // TODO yet more validation
+  if (typeof request.params.scale === 'string') {
+    request.params.scale = request.params.scale.toUpperCase();
+  } else {
+    request.params.scale = 'F';
+  }
+
+  // TODO more validation to consolidate
+  request.params.units = (typeof request.params.scale === 'string' && request.params.scale === 'C') ? 'si' : 'us'
+
+  var wr = new Weather10kbRequest(request);
+
+  wr.geocode()
+    .then(wr.setTimeZone)
+    .then(wr.getForecast)
+    .then(function(data) {
+      response.render('pages/index', objectMerge(JSON.parse(data), {params: request.params}));
+    })
+    .catch(function(err){
+      // TODO pretty clientside error handling
+      console.log(err);
+      response.send(err);
+    });
+});
+
+module.exports = router;
