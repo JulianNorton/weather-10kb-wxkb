fc5addc4d786f45e0d015d5ad01a25bc694ffaf8
Julian Norton <Julian@JulianNorton.com>
2016-09-27 17:24:17 -0400

    enabling opbeat express error middleware

--- index.js
+++ index.js
@@ -22,6 +22,9 @@ app.use(minifyHTML({
 }));
 app.use(express.static(__dirname + '/public'));
 app.use('/', weather10kb);
+// Add the Opbeat middleware after your regular middleware
+app.use(opbeat.middleware.express()) // injects opbeat, if error, registers in opbeat
+// https://opbeat.com/docs/articles/get-started-with-express/#express-errors
 
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'ejs');
