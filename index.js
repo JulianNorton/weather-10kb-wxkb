4414ae6019412b00302280ad6c94f9b9bca95eec
Tardog <buckling.spring@tutanota.com>
2016-10-26 21:52:14 +0200

    Refactoring to please the linter

--- index.js
+++ index.js
@@ -1,37 +1,39 @@
-var compression = require('compression');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var moment = require('moment-timezone');
-var minifyHTML = require('express-minify-html');
-var weather10kb = require('./weather10kb');
-var opbeat = require('opbeat')
+const compression = require('compression');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const moment = require('moment-timezone');
+const minifyHTML = require('express-minify-html');
+const weather10kb = require('./weather10kb');
+const opbeat = require('opbeat');
 
-var app = express();
+const app = express();
 
 app.locals.moment = moment;
 
 app.use(compression());
 app.use(minifyHTML({
-  override:      true,
+  override: true,
   htmlMinifier: {
-    removeComments:            true,
-    collapseWhitespace:        true,
+    removeComments: true,
+    collapseWhitespace: true,
     collapseBooleanAttributes: true,
-    removeAttributeQuotes:     true,
-    removeEmptyAttributes:     true,
-    minifyJS:                  true
-  }
+    removeAttributeQuotes: true,
+    removeEmptyAttributes: true,
+    minifyJS: true,
+  },
 }));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 app.use(cookieParser());
 app.use('/', weather10kb);
+
 // Add the Opbeat middleware after your regular middleware
-app.use(opbeat.middleware.express()) // injects opbeat, if error, registers in opbeat
+// injects opbeat, if error, registers in opbeat
+app.use(opbeat.middleware.express());
 // https://opbeat.com/docs/articles/get-started-with-express/#express-errors
 
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'ejs');
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
