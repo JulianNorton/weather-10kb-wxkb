c81a231554b68bc2961b03bf9114a3a1c25d6dcb
Tardog <buckling.spring@tutanota.com>
2016-10-28 17:57:02 +0200

    Use arrow-style for function

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 const letsEncryptReponse = process.env.CERTBOT_RESPONSE;
 
 // Return the Let's Encrypt certbot response:
-app.get('/.well-known/acme-challenge/:content', function(req, res) {
+app.get('/.well-known/acme-challenge/:content', (req, res) => {
   res.send(letsEncryptReponse);
 });
 
