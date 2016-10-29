16ab2e04a8ff527fde5a90e86f8d79b7af5a2b3c
Julian Norton <Julian@JulianNorton.com>
2016-10-29 11:41:52 -0400

    Revert "Merge pull request #107 from JulianNorton/development"
    This reverts commit 5181ea481e31ddda24a4c99e8d27c612fecc1623, reversing
    changes made to 30da750332000519e6d2e8cc3d6b0e4dcfcf6154.

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const compression = require('compression');
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const moment = require('moment-timezone');
-const minifyHTML = require('express-minify-html');
-const weather10kb = require('./weather10kb');
-const opbeat = require('opbeat');
+var compression = require('compression');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var moment = require('moment-timezone');
+var minifyHTML = require('express-minify-html');
+var weather10kb = require('./weather10kb');
+var opbeat = require('opbeat')
 
-const app = express();
+var app = express();
 
 // Read the Certbot response from an environment variable; we'll set this later:
 const letsEncryptReponse = process.env.CERTBOT_RESPONSE;
 
 // Return the Let's Encrypt certbot response:
-app.get('/.well-known/acme-challenge/:content', (req, res) => {
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
   res.send(letsEncryptReponse);
 });
 
@@ -20,28 +20,26 @@ app.locals.moment = moment;
 
 app.use(compression());
 app.use(minifyHTML({
-  override: true,
+  override:      true,
   htmlMinifier: {
-    removeComments: true,
-    collapseWhitespace: true,
+    removeComments:            true,
+    collapseWhitespace:        true,
     collapseBooleanAttributes: true,
-    removeAttributeQuotes: true,
-    removeEmptyAttributes: true,
-    minifyJS: true,
-  },
+    removeAttributeQuotes:     true,
+    removeEmptyAttributes:     true,
+    minifyJS:                  true
+  }
 }));
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(__dirname + '/public'));
 app.use(cookieParser());
 app.use('/', weather10kb);
-
 // Add the Opbeat middleware after your regular middleware
-// injects opbeat, if error, registers in opbeat
-app.use(opbeat.middleware.express());
+app.use(opbeat.middleware.express()) // injects opbeat, if error, registers in opbeat
 // https://opbeat.com/docs/articles/get-started-with-express/#express-errors
 
 app.set('port', (process.env.PORT || 5000));
 app.set('view engine', 'ejs');
 
-app.listen(app.get('port'), () => {
+app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
