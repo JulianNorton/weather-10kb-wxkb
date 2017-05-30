100be11e2b0d214ecda43595faaa765db0551c4a
Tardog <buckling.spring@mailbox.org>
2017-05-30 21:24:16 +0200

    Add integration tests with Chai and Mocha.

--- index.js
+++ index.js
@@ -1,45 +1,6 @@
-var compression = require('compression');
-var express = require('express');
-var cookieParser = require('cookie-parser');
-var moment = require('moment-timezone');
-var minifyHTML = require('express-minify-html');
-var weather10kb = require('./weather10kb');
-var opbeat = require('opbeat')
+const app = require('./src/app');
 
-var app = express();
-
-// Read the Certbot response from an environment variable; we'll set this later:
-const letsEncryptReponse = process.env.CERTBOT_RESPONSE;
-
-// Return the Let's Encrypt certbot response:
-app.get('/.well-known/acme-challenge/:content', function(req, res) {
-  res.send(letsEncryptReponse);
-});
-
-app.locals.moment = moment;
-
-app.use(compression());
-app.use(minifyHTML({
-  override:      true,
-  htmlMinifier: {
-    removeComments:            true,
-    collapseWhitespace:        true,
-    collapseBooleanAttributes: true,
-    removeAttributeQuotes:     true,
-    removeEmptyAttributes:     true,
-    minifyJS:                  true
-  }
-}));
-app.use(express.static(__dirname + '/public'));
-app.use(cookieParser());
-app.use('/', weather10kb);
-// Add the Opbeat middleware after your regular middleware
-app.use(opbeat.middleware.express()) // injects opbeat, if error, registers in opbeat
-// https://opbeat.com/docs/articles/get-started-with-express/#express-errors
-
-app.set('port', (process.env.PORT || 5000));
-app.set('view engine', 'ejs');
-
+// Start the server - set PORT in your environment to change the default port
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
