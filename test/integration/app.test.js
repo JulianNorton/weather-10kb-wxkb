aa64a8a1042cfa6e0c48750438e79210711411f3
Tardog <buckling.spring@mailbox.org>
2017-10-02 14:54:04 +0200

    Minor spacing fix

--- test/integration/app.test.js
+++ test/integration/app.test.js
@@ -10,6 +10,7 @@ chai.use(chaiHttp);
 const app = require('../../src/app');
 
 describe('Weather10kb app', function () {
+
   // TODO: Optimize the output to make this test pass
   // Skip the 10kb criterion for now
   xit('should deliver a body with a maximum size of 10kb', done => {
@@ -21,8 +22,8 @@ describe('Weather10kb app', function () {
       });
   });
 
-  it('should pass tenon.io accessibility tests', done => {
 
+  it('should pass tenon.io accessibility tests', done => {
     chai.request(app)
     .get('/')
     .end((err, res) => {
