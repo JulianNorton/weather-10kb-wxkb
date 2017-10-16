60bb7f3e704c39117ec3c8439aea4fd56f24291d
Vadym <vadym.ruchka@gmail.com>
2017-10-17 01:15:27 +0300

    Changing units does not update units on first click #188
    Code style fixes

--- public/sw-registration.js
+++ public/sw-registration.js
@@ -1,14 +1,14 @@
+function registerServiceWorker() {
+  navigator.serviceWorker.register('/sw.js', {scope: '/'}).then(function (registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    window.removeEventListener('load', registerServiceWorker);
+  }).catch(function (err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', registerServiceWorker);
-
-  function registerServiceWorker() {
-    navigator.serviceWorker.register('/sw.js', {scope: '/'}).then(function (registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      window.removeEventListener('load', registerServiceWorker);
-    }).catch(function (err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  }
 }
